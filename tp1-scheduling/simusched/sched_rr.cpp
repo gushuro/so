d3343114a6718138f7befb97eb29aeed2474de4f
gus <gushurovich@gmail.com>
2016-08-27 18:41:59 -0300

    round batman

--- tp1-scheduling/simusched/sched_rr.cpp
+++ tp1-scheduling/simusched/sched_rr.cpp
@@ -8,6 +8,11 @@ using namespace std;
 
 SchedRR::SchedRR(vector<int> argn) {
 	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
+	quantums = vector<int> (argn[0]);
+	ticksleft = vector<int> (argn[0]);
+	for (int i = 0; i < argn[0]; ++i) {
+		quantums[i] = ticksleft[i] = argn[i+1];
+	}
 }
 
 SchedRR::~SchedRR() {
@@ -17,13 +22,55 @@ SchedRR::~SchedRR() {
 
 void SchedRR::load(int pid) {
 /* completar */
+	q.push(pid); // llegó una tarea nueva
 }
 
 void SchedRR::unblock(int pid) {
+	q.push(pid); 	// sale de bloqueo, va a la cola de readys
 /* completar */
 }
 
 int SchedRR::tick(int cpu, const enum Motivo m) {
 /* completar */
+	if (m == EXIT){
+		// Si el proceso terminó, lo saco de la cola y sigo.
+		if (q.empty()) return IDLE_TASK;
+		else{
+			int sig = q.front(); q.pop();
+			ticksleft[cpu] = quantums[cpu];
+			return sig;
+		}
+
+	} else if (m == TICK){
+		//Si estaba idle y hay tarea, correla
+		if (current_pid(cpu) == IDLE_TASK && !q.empty()){
+			int sig = q.front();
+			q.pop();
+			return sig;
+		}
+		// Si hay una tarea corriendo:
+		if (ticksleft[cpu] == 0 && !q.empty()) {
+			// si no le quedan ticks y hay otra esperando, al fondo.
+			int sig = q.front();
+			q.pop();
+			q.push(current_pid(cpu));
+			ticksleft[cpu] = quantums[cpu];
+			return sig;
+		} else if (ticksleft[cpu] == 0) {
+			// no le quedan ticks pero la cola está vacía.
+			return current_pid(cpu);
+		} else {
+			// le quedan ticks: descuento 1 y sigue el mismo.
+			ticksleft[cpu]--;
+			return current_pid(cpu);
+		}
+	} else {
+		// block
+		if (q.empty()) return IDLE_TASK;	// si no hay más tareas, IDLE.
+		int sig = q.front();
+		q.pop();
+		ticksleft[cpu] = quantums[cpu];
+		return sig;
+	}
 	return 0;
 }
