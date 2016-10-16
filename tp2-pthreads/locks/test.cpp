08bfb39681d2aea033da5e93ddff97daebc3a674
Julian Palladino <julianpalladino@hotmail.com>
2016-10-16 14:59:00 -0300

    Agregado primer test de RWLock, anda bien

--- tp2-pthreads/locks/test.cpp
+++ tp2-pthreads/locks/test.cpp
@@ -4,7 +4,7 @@
 #include <pthread.h>
 #include "RWLock.h"
 #include <sys/types.h>
-
+#include <cstring>
 
 using namespace std;
 
@@ -12,20 +12,28 @@ RWLock rwlock = RWLock();
 
 int contador;
 
+uint64_t gettid() {
+    pthread_t ptid = pthread_self();
+    uint64_t threadId = 0;
+    memcpy(&threadId, &ptid, std::min(sizeof(threadId), sizeof(ptid)));
+    return threadId;
+}
+
 void* threadEscritor(void* args){
   auto tid = gettid();
   int contadorLocal;
 
-
-  cout << "Thread Num " << tid << " - esta esperando para escribir" << endl;
+  //printf("Thread %lu esta esperando para escribir \n", tid);
   rwlock.wlock();
-    cout << "Thread Num " << tid << " - tiene el recurso para escribir" << endl;
+    printf("Thread %lu tiene el recurso para escribir \n", tid);
     // el thread hace uso del mutex:
     sleep(1); // hace sleep para que no pase tan rapido
     contador++; // hace uso del recurso compartido
     contadorLocal = contador;
-    cout << "Thread Num " << tid << " - termino de escribir. Valor del contador: " << contadorLocal << endl;
+    printf("Thread %lu termino de escribir. Valor del contador: %i \n", tid, contadorLocal);
   rwlock.wunlock();
+
+  return args;
 }
 
 void* threadLector(void* args){
@@ -33,60 +41,43 @@ void* threadLector(void* args){
 
   int contadorLocal;
 
+  //printf("Thread %lu esta esperando para leer \n", tid);
   rwlock.rlock();
-    cout << "Thread Num " << tid << " - esta esperando para leer" << endl;
+    printf("Thread %lu tiene el recurso para leer \n", tid);
     // el thread hace uso del mutex:
     sleep(1); // hace sleep para que no pase tan rapido
     contadorLocal = contador; // hace uso del recurso compartido
-    cout << "Thread Num " << tid << " - termino de leer. Valor del contador: " << contadorLocal << endl;
+    printf("Thread %lu termino de leer. Valor del contador: %i \n", tid, contadorLocal);
   rwlock.runlock();
+
+  return args;
 }
 
-void* test1(void* argv){
+void test1(){
   // Caso dos lectores y un escritor a la vez
   contador = 0;
-  int n = rand() % 20 + 20; // random entre 20 y 40
+  //int n = rand() % 20 + 20; // random entre 20 y 40
+  int n = 10;
 
-  vector<int> threads = vector<int>(n*3);
+  printf("Este test creará 10 threads.\n");
+
+  pthread_t *threads;
+  threads = (pthread_t*)calloc(n*3, sizeof(pthread_t));
   for(int i = 0; i < n*3; i=i+3){
-    pthread_create(&threads[i], NULL, threadLector, (void*)&threads[i]);
-    pthread_create(&threads[i+1], NULL, threadLector, (void*)&threads[i+1]);
-    pthread_create(&threads[i+2], NULL, threadEscritor, (void*)&threads[i+2]);
+    pthread_t* p1 = &threads[i];
+    pthread_create(&threads[i], NULL, threadLector, p1);
+    pthread_t* p2 = &threads[i+1];
+    pthread_create(&threads[i+1], NULL, threadLector, p2);
+    pthread_t* p3 = &threads[i+2];
+    pthread_create(&threads[i+2], NULL, threadEscritor, p3);
   }
-}
-
-void* rwInteger(void* argv){
-  unsigned int id = pthread_self();
-  int random = rand() % 3;
-    if(!random){
-      rwlock.wlock();
-      cout << "El thread " << id << " quiere escribir" << endl;
-      sleep(1);
-      cout << "El thread " << id << " sale" << endl;
-      rwlock.wunlock();
-    } else {
-      rwlock.rlock();
-      cout << "El thread " << id << " quiere leer la variable compartido" << endl;
-      sleep(1);
-      cout << "El thread " << id << " sale" << endl;
-      rwlock.runlock();
-    }
-  pthread_exit(NULL);
-  return argv;
+  pthread_exit(NULL); // necesario para que no termine antes que sus threads hijos
 }
 
 
 int main(int argc, char const *argv[]) {
 
   test1();
-  /*pthread_t t1[NUMBER_THREADS];
-
-  for(int i = 0 ; i < NUMBER_THREADS; i++) 
-    pthread_create(&t1[i], NULL, &rwInteger, NULL) ;
-
-  cout << "Todos los threads creados" << endl;
-  for(int i = 0 ; i < NUMBER_THREADS; i++) 
-    pthread_join(t1[i], NULL);*/
 
   return 0;
 }
